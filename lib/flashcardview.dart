b354d553a1d6841059bcb88f0d53ddd93a3b8850
UP968909 <91615547+UP968909@users.noreply.github.com>
2023-05-08 01:10:34 +0100

    patterned the screen and added features

--- lib/flashcardview.dart
+++ lib/flashcardview.dart
@@ -7,11 +7,16 @@ class FlashCardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+        color: Colors.blue[400],
         elevation: 8.0,
         child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(fontSize: 25),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              textAlign: TextAlign.center,
+              text,
+              style: const TextStyle(fontSize: 25, color: Colors.black),
+            ),
           ),
         ));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:edcom/flashcard.dart';
 import 'package:edcom/flashcardview.dart';
+import 'package:edcom/home_screen.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,63 +19,17 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          colorSchemeSeed: Colors.blue,
           useMaterial3: true,
-          pageTransitionsTheme: const PageTransitionsTheme(builders: {
-            TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
-            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-          }),
+          pageTransitionsTheme: const PageTransitionsTheme(
+            builders: {
+              TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
+              TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            },
+          ),
         ),
         themeAnimationCurve: Curves.bounceIn,
         debugShowCheckedModeBanner: false,
-        title: 'FLashacard app',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Quiz Flashcards'),
-            ),
-            body: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 450,
-                      width: 300,
-                      child: FlipCard(
-                        direction: FlipDirection.HORIZONTAL,
-                        speed: 900,
-                        back: FlashCardView(
-                          text: 'OREEEHHH',
-                        ),
-                        front: Card(
-                            elevation: 8.0,
-                            child: Center(
-                              child: Text(
-                                'Who is a bad boy',
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            )),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 80.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        OutlinedButton.icon(
-                            onPressed: () {},
-                            icon: const Icon(Icons.arrow_back_ios),
-                            label: const Text('Previous')),
-                        const SizedBox(
-                          width: 80,
-                        ),
-                        OutlinedButton.icon(
-                            onPressed: () {},
-                            icon: const Icon(Icons.arrow_forward_ios),
-                            label: const Text('Next'))
-                      ],
-                    )
-                  ]),
-            )));
+        title: 'Flashcard app',
+        home: const HomeScreen());
   }
 }
